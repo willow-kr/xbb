f7cb8ba2174bc678d1301f7a9546b2976390aba6
willow-kr <willow@naver.com>
2023-05-10 16:43:51 +0900

    2023-05-10 소스 업데이트

--- xbb/src/main/java/com/mysite/xbb/HelloLombok.java
+++ xbb/src/main/java/com/mysite/xbb/HelloLombok.java
@@ -2,18 +2,20 @@ package com.mysite.xbb;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Getter
 @Setter
 public class HelloLombok {
 	
-	private String hello;
-	private int lombok;
+	private final String hello;
+	private final int lombok;
 	
 	public static void main(String[] args) {
-		HelloLombok hlombok = new HelloLombok();
-		hlombok.setHello("헬로");
-		hlombok.setLombok(10);
+		HelloLombok hlombok = new HelloLombok("헬로", 10);
+//		hlombok.setHello("헬로");
+//		hlombok.setLombok(10);
 		
 		System.out.println(hlombok.getHello());
 		System.out.println(hlombok.getLombok());
---
+++ xbb/src/main/java/com/mysite/xbb/MainController.java
@@ -0,0 +1,17 @@
+package com.mysite.xbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/sbb")
+	@ResponseBody
+	public String index() {
+		System.out.println("index");
+//		return "index";
+		return "안녕하세요 sbb에 오신것을 환영합니다.";
+	}
+}
