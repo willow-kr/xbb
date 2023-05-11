7df4d88a866106adf8d6ef304b2496d1798053f2
willow-kr <willow@naver.com>
2023-05-11 14:18:56 +0900

    2023-05-11 log4j 추가 함

--- xbb/src/test/java/com/mysite/xbb/XbbApplicationTests.java
+++ xbb/src/test/java/com/mysite/xbb/XbbApplicationTests.java
@@ -1,13 +1,63 @@
 package com.mysite.xbb;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootTest
 class XbbApplicationTests {
+	private static final Logger logger = LogManager.getLogger(XbbApplicationTests.class);
 
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	//@Test
+	void testJpa_1() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+		
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
+	}
+	
+	//@Test
+	void testJpa_2() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+		
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+	
+	@Test
+	void testJpa_3() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		
+		logger.info("[testJpa_3] Started");
+		if(oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+		logger.info("[testJpa_3] Ended");
 	}
 
 }
